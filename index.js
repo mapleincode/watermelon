052853079fda7693fef8204cda1fdd48aac5cf26
mapleincode <llkj13579@126.com>
2018-03-29 23:44:01 +0800

    feat: commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by maplex on 2018/3/29.
+ */
+const Doc = require('./lib/Doc');
+const H1
+
+module.exports = {
+    Doc,
+    H1
+};
---
+++ test.js
@@ -0,0 +1,3 @@
+/**
+ * Created by maplex on 2018/3/29.
+ */
